c8399888aead78bb27c68d5eaf7559795f57f401
gillianrhett <gillianrhett@gmail.com>
2026-05-19 18:41:36 -0700

    Milestone 2

--- 210-final-1.cpp
+++ 210-final-1.cpp
@@ -4,15 +4,13 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 int main() {
-// Milestone 1: In this assignment's details page, download and install the text file as your data source for this project. In it, each line contains two airport codes representing a particular flight's origin and destination, for example:
-// JFK LAX
-// Create an std::map<std::string, int> that maps an airport code to its traffic count. Read the file, and for each flight, insert origin and destination airports into the map if they do not exist yet; and increment each airport’s count once.
-//    Your driver program should build the map from the file and print all airports and their counts in the natural sorted order of the map. 
-    
+
+    // Milestone 1
     map<string, int> airports;
     ifstream in_file;
     in_file.open("210-final-1-SP26.txt");
@@ -27,13 +25,29 @@ int main() {
         else
             airports[str_temp]++;
     }
-
     cout << "All airport traffic counts:" << endl;
     for (auto pair : airports)
         cout << pair.first << " " << pair.second << endl;
 
-// Milestone 2: Add code that finds the highest traffic count to represent the busiest airport. Print all airports that have this maximum count (in other words, handle ties).
-//     Your driver program should build the map, print all counts, and then print the busiest airport(s) and their count. 
+    // Milestone 2:
+    int highest = 0;
+    for (auto pair : airports)
+        if (pair.second > highest)
+            highest = pair.second;
+    vector<string> busiest;
+    for (auto pair : airports)
+        if (pair.second == highest)
+            busiest.push_back(pair.first);
+    cout << "Busiest airport(s) with count " << highest << ": ";
+    int i = 0;
+    for (string airport : busiest) {
+        cout << airport << " (" << highest << ")";
+        if (i < busiest.size() - 1)
+            cout << ", ";
+        else
+            cout << endl;
+        ++i;
+    }
 
 // Milestone 3: Add a function that prints only airports whose traffic counts fall within a specified inclusive range [low, high].
 //    Your driver program should build the map and call the function at least twice with different ranges, clearly showing which airports fall in each range. Do not code user input. 
