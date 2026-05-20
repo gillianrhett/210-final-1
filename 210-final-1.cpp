ae4701c5a87f4ab78a5d25f3ecbb3a3b02fcde9d
gillianrhett <gillianrhett@gmail.com>
2026-05-19 18:51:00 -0700

    Milestone 3

--- 210-final-1.cpp
+++ 210-final-1.cpp
@@ -8,6 +8,8 @@
 
 using namespace std;
 
+void display_in_range(int, int, const map<string, int>&);
+
 int main() {
 
     // Milestone 1
@@ -38,7 +40,7 @@ int main() {
     for (auto pair : airports)
         if (pair.second == highest)
             busiest.push_back(pair.first);
-    cout << "Busiest airport(s) with count " << highest << ": ";
+    cout << "\nBusiest airport(s) with count " << highest << ": " << endl;
     int i = 0;
     for (string airport : busiest) {
         cout << airport << " (" << highest << ")";
@@ -48,10 +50,21 @@ int main() {
             cout << endl;
         ++i;
     }
+    cout << endl;
 
 // Milestone 3: Add a function that prints only airports whose traffic counts fall within a specified inclusive range [low, high].
 //    Your driver program should build the map and call the function at least twice with different ranges, clearly showing which airports fall in each range. Do not code user input. 
+    display_in_range(10, 20, airports);
+    display_in_range(4, 12, airports);
 
 // Milestone 4: Final polishing, and make sure your driver program exercises all the milestones of this assignment.
     return 0;
+}
+
+void display_in_range(int low, int high, const map<string, int>& map_in) {
+    cout << "Airports with traffic in range [" << low << ", " << high << "]: " << endl;
+    for (auto pair : map_in)
+        if (low <= pair.second && pair.second <= high)
+            cout << pair.first << " " << pair.second << endl;
+    cout << endl;
 }
